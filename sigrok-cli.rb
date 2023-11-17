6e3546bc4eb18392cec7a1b7875c0240b8acf0bb
Yoshinori Takesako <takesako@gmail.com>
2023-11-17 22:56:20 +0900

    Update sigrok-cli.rb

--- sigrok-cli.rb
+++ sigrok-cli.rb
@@ -1,10 +1,9 @@
-# Based on rob-deutsch's branch
-# https://github.com/rob-deutsch/homebrew-core/tree/sigrok
 class SigrokCli < Formula
   desc "Command-line frontend for sigrok"
   homepage "https://sigrok.org/wiki/Sigrok-cli"
   url "https://sigrok.org/download/source/sigrok-cli/sigrok-cli-0.7.0.tar.gz"
   sha256 "5669d968c2de3dfc6adfda76e83789b6ba76368407c832438cef5e7099a65e1c"
+  license "GPL-3.0-or-later"
   head "git://sigrok.org/sigrok-cli"
 
   depends_on "automake" => :build
@@ -14,9 +13,10 @@ class SigrokCli < Formula
   depends_on "pkg-config" => :build
   depends_on "takesako/sigrok/libsigrok"
   depends_on "takesako/sigrok/libsigrokdecode"
+  depends_on "takesako/sigrok/sigrok-firmware-fx2lafw"
 
   def install
-    if !File.exist?("configure") && File.exist?("autogen.sh") then
+    if build.head?
       system "./autogen.sh"
     end
     system "./configure", *std_configure_args, "--disable-silent-rules"
