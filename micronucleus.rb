374a3f64ab11af75dc11cdbbd5e0884690234c83
Yoshinori Takesako <takesako@gmail.com>
2024-05-15 21:23:19 +0900

    brew install --HEAD micronucleus

--- micronucleus.rb
+++ micronucleus.rb
@@ -4,15 +4,16 @@ class Micronucleus < Formula
   url "https://github.com/micronucleus/micronucleus/archive/refs/tags/v2.6.zip"
   sha256 "db31ca472647b6399cdbadb042557d9149ca9015dc4cc6e48b74a7b4499847da"
   license "GPL-2.0-or-later"
+  head "https://github.com/micronucleus/micronucleus.git"
 
-  depends_on "libusb"
   depends_on "libusb-compat"
 
+  stable do
+    patch :DATA
+  end
+
   def install
-    cd "./commandline" do
-      File.open("library/micronucleus_lib.h", "a") do |file|
-        file.puts '#define MICRONUCLEUS_COMMANDLINE_VERSION ("micronucleus-cli version: 2.6")'
-      end
+    cd "commandline" do
       ENV["USBFLAGS"] = `libusb-config --cflags`.chomp
       ENV["USBLIBS"]  = `libusb-config --libs`.chomp
       system "make"
@@ -23,4 +24,11 @@ class Micronucleus < Formula
   test do
     system "#{bin}/micronucleus", "--help"
   end
+  
 end
+
+__END__
+--- _head_/commandline/library/micronucleus_lib.h  2024-05-08 20:46:49
++++ stable/commandline/library/micronucleus_lib.h  2024-05-15 21:12:27
+@@ -124,0 +125 @@
++#define MICRONUCLEUS_COMMANDLINE_VERSION ("micronucleus-cli version: 2.6")
